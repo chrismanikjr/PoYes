dff1f896355e57685d3c5c0f3c2d98049db92b45
Chris Manik <chris.manikjr@gmail.com>
2022-04-28 08:25:05 +0700

    Add Color Asset and Setup Tab Bar

---
+++ Poyes/CalendarCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CalendarCollectionViewCell.swift
+//  Poyes
+//
+//  Created by Chrismanto Natanail Manik on 28/04/22.
+//
+
+import UIKit
+
+class CalendarCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var dayLabel: UILabel!
+}
